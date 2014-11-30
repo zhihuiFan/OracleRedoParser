9e29ca333d14c799af32ed4d5a69e64449037822
Fan Zhihui <zhifan@ebay.com>
2014-11-30 11:54:56 +0800

    more test case

--- src/redo_parse/test_case.sql
+++ src/redo_parse/test_case.sql
@@ -16,12 +16,29 @@ delete from target where a=2;
 delete from target where a=1;
 commit;
 
-insert into target(a, b, c)  select object_id, object_name, LAST_DDL_TIME from dba_objects where rownum < 8;
+insert into target(a, b, c)  select object_id, object_name, LAST_DDL_TIME from dba_objects where rownum < 3;
+commit;
+
+-- Mulit-Insert
+-- This will not generate Mulit-Insert, row too long?
+insert all
+    into target(a, b, c) values(-100, 'ab', sysdate-1)
+    into target(a) values(-101)
+    into target(a, c) values(-102, sysdate-2)
+select * from dual;
+drop table test;
+create table test (a int primary key, b varchar2(40), c date, d varchar2(40), e varchar2(40));
+-- This will generate Mulit-Insert
+insert all
+   into test values(-1, 'a', sysdate, 'a', 'a')
+   into test(a) values(-2)
+   into test(a,c) values(-3, sysdate-1)
+select * from dual;
 commit;
 -- all the Test passed @ d27167f93825c15629c789f464d0c534b5b6cce5
 
 
--- Row Chaining
+-- Row Chaining, not support so far
 insert into target values(-1, lpad('a', 4000, 'a'),  sysdate, lpad('b', 4000, 'b'), lpad('c', 4000, 'c'));
 commit;
 
