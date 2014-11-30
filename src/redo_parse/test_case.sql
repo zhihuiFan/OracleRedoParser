9a3f474135ba782838d53a763b10335970e7ec6f
Fan Zhihui <zhifan@ebay.com>
2014-11-30 09:44:28 +0800

    test_case improved

--- src/redo_parse/test_case.sql
+++ src/redo_parse/test_case.sql
@@ -18,12 +18,15 @@ commit;
 
 insert into target(a, b, c)  select object_id, object_name, LAST_DDL_TIME from dba_objects where rownum < 8;
 commit;
-alter system switch logfile;
-!sleep 5
-select name from (select name from v$archived_log order by sequence# desc) where rownum < 4;
 -- all the Test passed @ d27167f93825c15629c789f464d0c534b5b6cce5
 
 
 -- Row Chaining
 insert into target values(-1, lpad('a', 4000, 'a'),  sysdate, lpad('b', 4000, 'b'), lpad('c', 4000, 'c'));
 commit;
+
+
+
+alter system switch logfile;
+!sleep 5
+select name from (select name from v$archived_log order by sequence# desc) where rownum < 4;
