fc264c9a0c4a9330b488d3f807a7dd7fa2ca986d
Fan Zhihui <zhifan@ebay.com>
2014-12-17 20:56:04 +0800

    rewrite dumpop

--- src/redo_parse/dump_op.cpp
+++ src/redo_parse/dump_op.cpp
@@ -18,39 +18,21 @@
 #include <stdlib.h>
 #include <map>
 #include <iostream>
-#include "workers.h"
 #include "redofile.h"
 #include "logical_elems.h"
-#include "physical_elems.h"
 
-namespace databus {
-  SList record_pointers;
-  RecordList record_bufs;
+int main(int argc, char** argv) {
+  std::map<short, uint32_t> opmap;
+  databus::RedoFile redofile(argv[1]);
+  databus::RecordBuf* buf = NULL;
+  while ((buf = redofile.nextRecordBuf()) != NULL) {
+    for (auto change : buf->change_vectors) {
+      opmap[change->opCode()] += 1;
+    }
+  }
 
-  int main(int argc, char** argv) {
-    Reader reader(&record_pointers);
-    reader.feed(argv[1]);
-    reader.srun();
-    std::cout << "total record " << record_pointers.size() << std::endl;
-    std::map<Ushort, unsigned long> total_count;
-    RecordBuilder builder(&reader, &record_bufs);
-    builder.srun();
-    std::cout << "build record " << record_bufs.size() << std::endl;
-    while (!record_bufs.empty()) {
-      RecordBuf* record_buf = record_bufs.pop_front();
-      if (record_buf != NULL) {
-        for (auto change : record_buf->change_vectors) {
-          if (change != NULL) {
-            Ushort opcode = change->opCode();
-            total_count[opcode]++;
-          }
-        }
-      }
-    }
-    for (auto i : total_count) {
-      std::cout << std::hex << i.first << "\t" << std::dec << i.second << "\n";
-    }
-    return 0;
+  for (auto i : opmap) {
+    std::cout << std::hex << "0x" << i.first << "->" << std::dec << i.second
+              << std::endl;
   }
 }
-int main(int argc, char** argv) { return databus::main(argc, argv); }
