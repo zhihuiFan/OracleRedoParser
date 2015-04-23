b736de179e75ace562bb5bb9937f1ce811ddc93e
Fan Zhihui <zhifan@ebay.com>
2015-04-23 16:16:07 +0800

    add 1 more test_case

--- src/redo_parse/test_case.sql
+++ src/redo_parse/test_case.sql
@@ -1,5 +1,4 @@
 --- Purpose:  Gather all the test cases for log mining 
---- Author :  Fan Zhihui
 --- Created:  2014/11/29
 conn andy/andy
 set echo on
@@ -7,6 +6,8 @@ drop table target;
 create table target (b varchar2(4000), a number(38,10) primary key, c date, d varchar2(4000), e varchar2(4000));
 drop table test;
 create table test (a int primary key, b varchar2(40), c date, d varchar2(40), e varchar2(40));
+drop table rc2;
+create table rc2(id int, c1 varchar2(3000), c2 varchar2(3000), c3 varchar2(3000), primary key(id,c2)); 
 
 alter system switch logfile;
 
@@ -58,6 +59,16 @@ commit;
 -- Row Migration
 insert into target(a) values(623);
 update target set b=lpad('9',4000,'9'), d=lpad('1', 4000, '1'), e=lpad('2', 4000, '2') where a=623;
+insert into target(a) values(1623);
+update target set a=2632, b=lpad('9',4000,'9'), d=lpad('1', 4000, '1'), e=lpad('2', 4000, '2') where a=1623;
+insert into target(a) values(1624);
+commit;
+
+insert into rc2 values(1, rpad('A',30,'A'), rpad('B',30,'B'), rpad('C',30,'C')); 
+commit;
+
+update rc2 set id=2, c1=rpad('A',3000,'A'), c2=rpad('B',3000,'B'), c3=rpad('C',3000,'C') where id=1; 
+commit;
 
 ---
 --- 5.2 -> 5.1 -> 11.2 (pk only)
@@ -67,7 +78,6 @@ update target set b=lpad('9',4000,'9'), d=lpad('1', 4000, '1'), e=lpad('2', 4000
 --- if row_chain is must:  ==> 11.2 ==> (0,e)
 ---                       pk is first, take 1 11.2
 
-commit;
 
 alter system switch logfile;
 !sleep 5
